370d1872b02bdf506b17f6123609c01445279593
Shawn Wildermuth <shawn@wildermuth.com>
2019-04-10 19:47:40 -0400

    module 02 doneUpdate version of Razor Design

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data.Entities.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c=> c.Venue, x=>x.MapFrom(a=>a.Location.VenueName))
+                ;
+        }
+    }
+}
---
+++ src/Models/TalkModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreCodeCamp.Models.Models;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CoreCodeCamp.Models
+{
+    public class TalkModel
+    {
+        public int TalkId { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public int Level { get; set; }
+
+        public SPeakerModel Speaker { get; set; }
+    }
+}
